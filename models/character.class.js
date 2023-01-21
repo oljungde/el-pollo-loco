f53e4c1092a5dc07dab4c8a7cacba1297ea22bfe
oljungde <oljung@posteo.de>
2023-01-21 18:06:52 +0100

    character movement

--- models/character.class.js
+++ models/character.class.js
@@ -8,6 +8,7 @@ class Character extends MovableObject {
         '../img/2_character_pepe/2_walk/W-26.png'
     ];
     world;
+    speed = 3;
 
 
     constructor() {
@@ -20,6 +21,15 @@ class Character extends MovableObject {
     animate() {
         setInterval(() => {
             if (this.world.keyboard.RIGHT) {
+                this.posX += this.speed;
+            }
+            if (this.world.keyboard.LEFT) {
+                this.posX -= this.speed;
+            }
+        }, 1000 / 60);
+
+        setInterval(() => {
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
                 let i = this.currentImage % this.IMAGES_WALKING.length;
                 let path = this.IMAGES_WALKING[i];
                 this.img = this.imageCache[path];
