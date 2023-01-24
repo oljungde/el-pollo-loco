45bd388613c5752f1808fc4b94a563363fc8ffcb
oljungde <oljung@posteo.de>
2023-01-24 06:50:16 +0100

    show jump animation

--- models/character.class.js
+++ models/character.class.js
@@ -7,6 +7,17 @@ class Character extends MovableObject {
         '../img/2_character_pepe/2_walk/W-25.png',
         '../img/2_character_pepe/2_walk/W-26.png'
     ];
+    IMAGES_JUMPING = [
+        'img/2_character_pepe/3_jump/J-31.png',
+        'img/2_character_pepe/3_jump/J-32.png',
+        'img/2_character_pepe/3_jump/J-33.png',
+        'img/2_character_pepe/3_jump/J-34.png',
+        'img/2_character_pepe/3_jump/J-35.png',
+        'img/2_character_pepe/3_jump/J-36.png',
+        'img/2_character_pepe/3_jump/J-37.png',
+        'img/2_character_pepe/3_jump/J-38.png',
+        'img/2_character_pepe/3_jump/J-39.png'
+    ]
     world;
     speed = 8;
     walkingAudio = new Audio('./../audio/walking.mp3')
@@ -15,6 +26,7 @@ class Character extends MovableObject {
     constructor() {
         super().loadImage('../img/2_character_pepe/2_walk/W-21.png');
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_JUMPING);
         this.animate();
         this.applyGravity();
     }
@@ -39,6 +51,9 @@ class Character extends MovableObject {
         setInterval(() => {
             if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
                 this.playAnimation(this.IMAGES_WALKING);
+            }
+            if (this.isAboveGround()) {
+                this.playAnimation(this.IMAGES_JUMPING);
             }
         }, 150);
     }
