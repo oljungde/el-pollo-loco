d05c7ba017bb9636d359cd79e3151db9de156379
oljungde <oljung@posteo.de>
2023-02-18 14:00:46 +0100

    mobile navigations buttons

--- js/game.js
+++ js/game.js
@@ -10,6 +10,8 @@ function init() {
     canvas = document.getElementById('canvas');
     world = new World(canvas, keyboard);
     console.log('My character is', world.character);
+    touchEventsStart();
+    toucheventsEnd();
 }
 
 
@@ -23,9 +25,9 @@ function startGame() {
     startscreen = document.getElementById('startscreen');
     world.character.animate();
     world.level.endboss.animate();
-    world.level.enemies.forEach(enemy => {
-        enemy.animate();
-    });
+    // world.level.enemies.forEach(enemy => {
+    //     enemy.animate();
+    // });
     world.level.clouds.forEach(cloud => {
         cloud.animate();
     });
@@ -80,3 +82,43 @@ window.addEventListener('keyup', (ev) => {
         keyboard.THROW = false;
     }
 });
+
+
+function touchEventsStart() {
+    document.getElementById('btnLeft').addEventListener('touchstart', (ev) => {
+        // ev.preventDefault();
+        keyboard.LEFT = true;
+    });
+    document.getElementById('btnRight').addEventListener('touchstart', (ev) => {
+        // ev.preventDefault();
+        keyboard.RIGHT = true;
+    });
+    document.getElementById('btnJump').addEventListener('touchstart', (ev) => {
+        // ev.preventDefault();
+        keyboard.UP = true;
+    });
+    document.getElementById('btnThrow').addEventListener('touchstart', (ev) => {
+        // ev.preventDefault();
+        keyboard.THROW = true;
+    });
+}
+
+
+function toucheventsEnd() {
+    document.getElementById('btnLeft').addEventListener('touchend', (ev) => {
+        // ev.preventDefault();
+        keyboard.LEFT = false;
+    });
+    document.getElementById('btnRight').addEventListener('touchend', (ev) => {
+        // ev.preventDefault();
+        keyboard.RIGHT = false;
+    });
+    document.getElementById('btnJump').addEventListener('touchend', (ev) => {
+        // ev.preventDefault();
+        keyboard.UP = false;
+    });
+    document.getElementById('btnThrow').addEventListener('touchend', (ev) => {
+        // ev.preventDefault();
+        keyboard.THROW = false;
+    });
+}
